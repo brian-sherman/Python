8390137e985349c4dfd6ac7d1d757a6041743469
Brian Sherman <bsocial@cloudantics.com>
2021-07-09 17:04:56 -0700

    Work on Section 7

---
+++ practice/C859 Intro to Python/Challenges/07 Functions/7_1_2_Basic_Function_Call.py
@@ -0,0 +1,20 @@
+"""
+
+Retype the following statements. Complete print_shape() to print the below shape.
+def print_shape():
+   # FIXME: complete function to print desired shape
+   return
+Example output:
+***
+***
+***
+
+"""
+
+def print_shape():
+   print("***")
+   print("***")
+   print("***")
+   return
+
+print_shape()
