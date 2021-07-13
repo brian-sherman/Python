c4f21addaebc12123de248cd74153e0949964a12
Brian Sherman <bsocial@cloudantics.com>
2021-07-12 20:54:17 -0700

    Section 7 Work

---
+++ practice/C859 Intro to Python/Challenges/07 Functions/7_3_2_Pyramid_Volume.py
@@ -0,0 +1,14 @@
+"""
+Define a function pyramid_volume with parameters base_length, base_width, and pyramid_height, 
+that returns the volume of a pyramid with a rectangular base.
+
+Relevant geometry equations:
+Volume = base area x height x 1/3
+Base area = base length x base width.
+"""
+
+
+def pyramid_volume(base_length, base_width, pyramid_height):
+    return (base_length * base_width) * pyramid_height * (1 / 3)
+
+print('Volume for 4.5, 2.1, 3.0 is:', pyramid_volume(4.5, 2.1, 3.0))
