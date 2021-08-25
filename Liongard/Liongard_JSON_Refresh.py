83404fea7165e008f583b828dabf82754a457cbb
Brian Sherman <bsocial@cloudantics.com>
2021-08-25 11:34:38 -0700

    defined get_data function and converted

--- Liongard/Liongard_JSON_Refresh.py
+++ Liongard/Liongard_JSON_Refresh.py
@@ -2,11 +2,8 @@ import requests
 import base64
 import json
 
-access_id = '***********'
-secret_key = '***********************'
-
-systems_url = "https://us1.app.liongard.com/api/v1/systems"
-inspectors_url = "https://us1.app.liongard.com/api/v1/launchpoints"
+access_id = '****************'
+secret_key = '**********************************'
 
 id_secret = access_id + ':' + secret_key
 enc_id_secret = base64.b64encode(id_secret.encode())
@@ -15,15 +12,20 @@ url_headers = {
     "X-ROAR-API-KEY":enc_id_secret,
 }
 
+def get_data(url, path):
+    response = requests.get(url, headers=url_headers)
+    json_response = json.loads(response.text)
+    with open(path, "w+") as f:
+        f.write(json.dumps(json_response, indent=4, sort_keys=True))
+
+systems_url = "https://us1.app.liongard.com/api/v1/systems"
+inspectors_url = "https://us1.app.liongard.com/api/v1/launchpoints"
+agents_url = "https://us1.app.liongard.com/api/v1/agents"
+
 systems_path = "C:\\Filepath\\Systems.json"
 inspectors_path = "C:\\Filepath\\Inspectors.json"
+agents_path = "C:\\Filepath\\Agents.json"
 
-systems_response = requests.get(systems_url, headers=url_headers)
-systems_json_response = json.loads(systems_response.text)
-with open(systems_path, "w+") as f:
-    f.write(json.dumps(systems_json_response, indent=4, sort_keys=True))
-
-inspectors_response = requests.get(inspectors_url, headers=url_headers)
-inspectors_json_response = json.loads(inspectors_response.text)
-with open(inspectors_path, "w+") as f:
-    f.write(json.dumps(inspectors_json_response, indent=4, sort_keys=True))
+get_data(systems_url, systems_path)
+get_data(inspectors_url, inspectors_path)
+get_data(agents_url, agents_path)
